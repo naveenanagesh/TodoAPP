0d1a6bf9495abac82dc3401f9be0700af5b13123
naveenanagesh <naveenanageshn@gmail.com>
2020-10-01 12:44:05 +0530

    frontend

---
+++ client/src/components/AddTodo.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { addTodo } from "../actions";
+
+class AddTodo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    console.log("input value:", e);
+    if (e.key === "Enter") {
+      this.props.addTodo(e.target.value);
+      e.target.value = "";
+    }
+  }
+
+  render() {
+    return <input onKeyPress={this.handleSubmit} />;
+  }
+}
+
+export default connect(null, { addTodo })(AddTodo);
